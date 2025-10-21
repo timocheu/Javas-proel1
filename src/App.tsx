5ca6d7c836118e0f18cce30fe2da88eb68f78de6
timocheu <ccs.timotheoche@gmail.com>
2025-10-21 16:06:17 +0800

    Refactor: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let accounts =;
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Counting: {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Typeshi
-      </p>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Shop Now</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+
+      <footer>
+      </footer>
     </>
   )
 }
