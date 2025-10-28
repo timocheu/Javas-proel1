ff47704470e805093f5f88f3334a419ec9288d77
timocheu <ccs.timotheoche@gmail.com>
2025-10-29 03:33:27 +0800

    Install tailwind for shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
+import Header from './Header.tsx'
 import './App.css'
 
 function App() {
-  let accounts =;
-  
   return (
     <>
-      <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Shop Now</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </nav>
-
-      <footer>
-      </footer>
     </>
   )
 }
